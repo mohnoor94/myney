afda381ca7e6b1888e0fdb5ec50237593f684d8c
mohnoor94 <moh.noor94@gmail.com>
2022-09-03 02:30:53 +0300

    refactor(Log): Use Proper Logging

--- src/main/kotlin/guru/noor/myney/api/v1/AccountController.kt
+++ src/main/kotlin/guru/noor/myney/api/v1/AccountController.kt
@@ -1,6 +1,8 @@
 package guru.noor.myney.api.v1
 
 import guru.noor.myney.model.Account
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +18,13 @@ class AccountController {
     @GetMapping("{id}")
     fun getAccount(@PathVariable id: String): Account {
         // TODO: implement
-        return Account.create("Noor", 100, "USD")
+        return Account.create("Noor", 100, "USD").also {
+            log.info("Returning account $it")
+        }
+    }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(AccountController::class.java)
     }
 
 }
--- src/main/kotlin/guru/noor/myney/api/v1/TransactionController.kt
+++ src/main/kotlin/guru/noor/myney/api/v1/TransactionController.kt
@@ -2,6 +2,8 @@ package guru.noor.myney.api.v1
 
 import guru.noor.myney.model.Transaction
 import guru.noor.myney.model.request.TransactionRequest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 
 /**
@@ -13,8 +15,13 @@ class TransactionController {
 
     @PostMapping
     fun sendMoney(@PathVariable id: String, @RequestBody transactionRequest: TransactionRequest): Transaction {
-        println("Transaction: $transactionRequest")
+        log.info("Transaction request from account {} with the details: {}", id, transactionRequest)
 
         return Transaction.from(transactionRequest)
+    }
+
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(TransactionController::class.java)
     }
 }
