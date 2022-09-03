6c070b7a50eb0cc3fa32a109dca94b8fcb673509
mohnoor94 <moh.noor94@gmail.com>
2022-09-03 19:38:49 +0300

    feat(DB): Add Database Table Scripts

---
+++ db/transactions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE
+  transactions (
+    id char(36) NOT NULL,
+    senderAccountId char(36) NOT NULL,
+    receiverAccountId char(36) NOT NULL,
+    amount decimal(10, 0) NOT NULL DEFAULT '0',
+    currency varchar(255) NOT NULL DEFAULT 'USD',
+    createdAt datetime NOT NULL,
+    PRIMARY KEY (id)
+  );
