6e367d87bb0799153e4dc2319d0eb542a6e65695
mohnoor94 <moh.noor94@gmail.com>
2022-09-04 00:16:15 +0300

    test(Context): Add Basic Tests for SpringBoot Controllers

--- src/test/kotlin/guru/noor/myney/ApplicationTests.kt
+++ src/test/kotlin/guru/noor/myney/ApplicationTests.kt
@@ -1,13 +1,22 @@
 package guru.noor.myney
 
+import guru.noor.myney.api.v1.AccountController
+import guru.noor.myney.api.v1.TransactionController
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class ApplicationTests {
+class ApplicationTests(
+    @Autowired private val accountController: AccountController,
+    @Autowired private val transactionController: TransactionController,
+) {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+        assertNotNull(accountController)
+        assertNotNull(transactionController)
+    }
 
 }
