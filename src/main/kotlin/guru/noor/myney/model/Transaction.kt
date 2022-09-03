c05932ceb904750034a6712d59ef36db61cb5afe
mohnoor94 <moh.noor94@gmail.com>
2022-09-03 19:39:37 +0300

    refactor(Specs): Update Specs and Classes

--- src/main/kotlin/guru/noor/myney/model/Transaction.kt
+++ src/main/kotlin/guru/noor/myney/model/Transaction.kt
@@ -12,7 +12,8 @@ import java.util.*
  * A transaction model.
  *
  * @property id the id of the transaction.
- * @property accountId the id of the receiving account.
+ * @property senderAccountId the id of the sender account.
+ * @property receiverAccountId the id of the receiver account.
  * @property amount the amount of the transaction.
  * @property currency the currency of the transaction.
  * @property createdAt the date & time of the transaction.
@@ -22,9 +23,13 @@ data class Transaction(
     @Schema(example = "1b3ef0159-6149-43b1-a54f-c75144e64931", required = true, description = "Transaction ID")
     val id: String?,
 
-    @JsonProperty("accountId")
-    @Schema(example = "1b3ef0159-6149-43b1-a54f-c75144e64931", required = true, description = "Transaction ID")
-    val accountId: String,
+    @JsonProperty("senderAccountId")
+    @Schema(example = "1b3ef0159-6149-43b1-a54f-c75144e64931", required = true, description = "Receiver Account ID")
+    val senderAccountId: String,
+
+    @JsonProperty("receiverAccountId")
+    @Schema(example = "f981810b-f774-475a-95a8-75090a2603db", required = true, description = "Receiver Account ID")
+    val receiverAccountId: String,
 
     @JsonProperty("amount")
     @Schema(example = "2500", required = true, description = "Transaction amount")
@@ -45,15 +50,22 @@ data class Transaction(
         /**
          * Creates a new transaction.
          *
-         * @param accountId the id of the receiving account.
+         * @param senderAccountId the id of the sending account.
+         * @param receiverAccountId the id of the receiving account.
          * @param amount the amount of the transaction.
          * @param currency the currency of the transaction.
          * @return a new transaction.
          */
-        fun create(accountId: String, amount: BigDecimal, currency: String): Transaction {
+        fun create(
+            senderAccountId: String,
+            receiverAccountId: String,
+            amount: BigDecimal,
+            currency: String,
+        ): Transaction {
             return Transaction(
                 id = UUID.randomUUID().toString(),
-                accountId = accountId,
+                senderAccountId = senderAccountId,
+                receiverAccountId = receiverAccountId,
                 amount = amount,
                 currency = currency,
                 createdAt = ZonedDateTime.now()
@@ -61,13 +73,19 @@ data class Transaction(
         }
 
         /**
-         * Creates a new transaction from a transaction request.
+         * Creates a new transaction from a transaction request and a sender accountID.
          *
-         *
+         * @param senderAccountId the id of the sending account.
+         * @param transactionRequest the transaction request.
+         * @return a new transaction.
          */
-        fun from(transactionRequest: TransactionRequest): Transaction {
+        fun from(
+            senderAccountId: String,
+            transactionRequest: TransactionRequest,
+        ): Transaction {
             return create(
-                accountId = transactionRequest.accountId,
+                senderAccountId = senderAccountId,
+                receiverAccountId = transactionRequest.receiverAccountId,
                 amount = transactionRequest.amount,
                 currency = transactionRequest.currency
             )
--- src/main/kotlin/guru/noor/myney/model/request/TransactionRequest.kt
+++ src/main/kotlin/guru/noor/myney/model/request/TransactionRequest.kt
@@ -4,10 +4,17 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import java.math.BigDecimal
 
+/**
+ * A representation of thr minimal required data to create a new transaction.
+ *
+ * @property receiverAccountId the id of the receiver account.
+ * @property amount the amount of the transaction.
+ * @property currency the currency of the transaction.
+ */
 data class TransactionRequest(
-    @JsonProperty("accountId")
-    @Schema(example = "1b3ef0159-6149-43b1-a54f-c75144e64931", required = true, description = "Transaction ID")
-    val accountId: String,
+    @JsonProperty("receiverAccountId")
+    @Schema(example = "f981810b-f774-475a-95a8-75090a2603db", required = true, description = "Receiver Account ID")
+    val receiverAccountId: String,
 
     @JsonProperty("amount")
     @Schema(example = "2500", required = true, description = "Transaction amount")
