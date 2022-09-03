78b92c52a88e1db6e7fd03c6d4133f92697111c4
mohnoor94 <moh.noor94@gmail.com>
2022-09-03 23:47:53 +0300

    refactor(database): Update Tables Scripts

--- db/accounts.sql
+++ db/accounts.sql
@@ -1,10 +1,10 @@
-CREATE TABLE
-  accounts (
-    id char(36) NOT NULL,
-    name varchar(255) NOT NULL,
-    balance decimal(10, 0) NOT NULL DEFAULT '0',
-    currency varchar(5) NOT NULL DEFAULT 'USD',
-    created_at datetime NOT NULL,
+CREATE TABLE accounts
+(
+    id         char(36)       NOT NULL,
+    name       varchar(255)   NOT NULL,
+    balance    decimal(10, 0) NOT NULL DEFAULT '0',
+    currency   varchar(5)     NOT NULL DEFAULT 'USD',
+    created_at datetime       NOT NULL,
     PRIMARY KEY (id)
-  );
+);
 
--- db/transactions.sql
+++ db/transactions.sql
@@ -1,10 +1,10 @@
-CREATE TABLE
-  transactions (
-    id char(36) NOT NULL,
-    senderAccountId char(36) NOT NULL,
-    receiverAccountId char(36) NOT NULL,
-    amount decimal(10, 0) NOT NULL DEFAULT '0',
-    currency varchar(255) NOT NULL DEFAULT 'USD',
-    created_at datetime NOT NULL,
+CREATE TABLE transactions
+(
+    id                  char(36)       NOT NULL,
+    sender_account_id   char(36)       NOT NULL,
+    receiver_account_id char(36)       NOT NULL,
+    amount              decimal(10, 0) NOT NULL DEFAULT '0',
+    currency            varchar(255)   NOT NULL DEFAULT 'USD',
+    created_at          datetime       NOT NULL,
     PRIMARY KEY (id)
-  );
+);
